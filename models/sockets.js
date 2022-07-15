0d438b95ce137c07d751ed63e4880d7cfda35b79
Hector Ibarra <hector@oneseventech.com>
2022-07-14 20:55:46 -0400

    sockets

--- models/sockets.js
+++ models/sockets.js
@@ -9,11 +9,7 @@ class Sockets {
     // On connection
     this.io.on("connection", (socket) => {
       // Escuchar evento: mensaje-to-server
-      socket.on("mensaje-to-server", (data) => {
-        console.log(data);
-
-        this.io.emit("mensaje-from-server", data);
-      });
+      console.log("Client connected");
     });
   }
 }
